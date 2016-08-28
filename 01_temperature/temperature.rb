d15ead64e826a2b2d768ec0cc1bb1138fcfe390b
Andrew <andrew.j.zeller@gmail.com>
2016-08-27 17:22:02 -0700

    this is where it all begins...

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+#converts temperature in fahrenheit to celcius
+
+def ftoc (temp)
+  (temp-32.0) * 5.0/9.0
+end
+
+#converts temperature in celcius to fahrenheit
+
+def ctof (temp)
+  temp * 9.0/5.0 + 32.0
+end
